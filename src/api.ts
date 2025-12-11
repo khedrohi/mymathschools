01afafcd3e4a7586b01f1fc2b29332e97e3c587e
Waseemishere <46986718+waseembader@users.noreply.github.com>
2025-12-11 23:28:04 +0530

    Update api.ts

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,1 @@
-const API_URL = import.meta.env.VITE_API_URL as string;
-export async function getStudents() {
-  const res = await fetch(`${API_URL}/students.php`);
-  if (!res.ok) throw new Error("API request failed");
-  return res.json();
-}
+
